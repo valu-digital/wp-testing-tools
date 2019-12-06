18e0bd2670901afcde906e897f121bead1c66eb5
Esa-Matti Suuronen <esa-matti@suuronen.org>
2019-12-06 19:57:21 +0200

    Add functional test example

--- plugin/src/Example.php
+++ plugin/src/Example.php
@@ -5,5 +5,9 @@ namespace Example;
 class Example {
     public static function init() {
         define( 'EXAMPLE', 'initialized' );
+
+        add_action('the_title', function () {
+            return 'EXAMPLE TITLE MOD';
+        });
     }
 }
