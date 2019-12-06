284ff909d66c707d416e99196924190ad1ce2c67
Esa-Matti Suuronen <esa-matti@suuronen.org>
2019-12-06 20:42:32 +0200

    Cleanup init

--- plugin/docker/init.sh
+++ plugin/docker/init.sh
@@ -9,11 +9,19 @@ fi
 
 composer install
 
+composer wp-install
+
 if [ -f init-docker.sh ]; then
     # Run the custom init if any
-    exec ./init-docker.sh
+    ./init-docker.sh
+fi
+
+if [ -f "${WP_TT_INSTALL_DIR}/web/wp-config.php" ]; then
+    >&2 echo "WP installed. You can access it from http://localhost:8080/ and run tests against it using ./docker/shell.sh"
+    cd "${WP_TT_INSTALL_DIR}/web"
+    exec wp server --host=0.0.0.0
 else
     # Otherwise just keep the container running so it can be accessed with docker/shell.sh
-    >&2 echo "Init ok! Start the shell in antoher terminal with docker/shell.sh"
+    >&2 echo "Init ok! Start the shell in antoher terminal with ./docker/shell.sh"
     exec tail -f /dev/null
 fi
--- plugin/init-docker.sh
+++ plugin/init-docker.sh
@@ -2,12 +2,6 @@
 
 set -eu
 
-composer wp-install
 
->&2 echo "WP installed. You can access it from http://localhost:8080/ and run tests against it using docker/shell.sh"
-
-# Start WordPress server with wp-cli for functional, acceptantce and manual
-# testing. The WP instance can be accessed from http://localhost:8080/
-cd .wp-install/web
-set -x
-exec wp server --host=0.0.0.0
+echo lol
+echo lol
